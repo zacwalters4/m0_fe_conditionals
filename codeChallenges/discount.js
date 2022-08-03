772921599c5e92070b03945e4c80a2980010a458
Zac Walters <zacwalters4@gmail.com>
2022-08-03 15:33:43 -0600

    Add discount.js

---
+++ codeChallenges/discount.js
@@ -0,0 +1,14 @@
+var goodDrivingRecord = false
+var isOver25 = false
+
+if (goodDrivingRecord && isOver25){
+  console.log("Have a discount :)");
+} else if (goodDrivingRecord){
+    console.log("As if you'd get a discount o_O");
+}
+  else if (isOver25){
+    console.log("Yeah, not giving you a discount :I");
+}
+  else{
+    console.log("Where's your mom sweety =|");
+  }
